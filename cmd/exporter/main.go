9fe8423ad25fb8892aabc79ced6625b273da473f
Praveen Kumar Shanmugam <58961022+spraveenio@users.noreply.github.com>
2024-12-11 19:04:32 -0800

    Move all the init code for easy consumption through imports (#177)

--- cmd/exporter/main.go
+++ cmd/exporter/main.go
@@ -17,221 +17,21 @@
 package main
 
 import (
-	"context"
-	"expvar"
 	"flag"
 	"fmt"
-	"log"
-	"net/http"
-	"net/http/pprof"
 	"os"
-	"path"
-	"strings"
-	"time"
 
-	"github.com/fsnotify/fsnotify"
-	"github.com/gorilla/mux"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-
-	"github.com/pensando/device-metrics-exporter/pkg/amdgpu/config"
+	"github.com/pensando/device-metrics-exporter/pkg/amdgpu/exporter"
 	"github.com/pensando/device-metrics-exporter/pkg/amdgpu/globals"
-	"github.com/pensando/device-metrics-exporter/pkg/amdgpu/gpuagent"
 	"github.com/pensando/device-metrics-exporter/pkg/amdgpu/logger"
-	"github.com/pensando/device-metrics-exporter/pkg/amdgpu/metricsutil"
-	metricsserver "github.com/pensando/device-metrics-exporter/pkg/amdgpu/svc"
 	"github.com/pensando/device-metrics-exporter/pkg/amdgpu/utils"
 )
 
-// single instance handlers
 var (
 	Version   string
 	BuildDate string
 	GitCommit string
-	mh        *metricsutil.MetricsHandler
-	gpuclient *gpuagent.GPUAgentClient
-	runConf   *config.ConfigHandler
 )
-
-const (
-	metricsHandlerPrefix = "/metrics"
-)
-
-// get the info from gpu agent and update the current metrics registery
-func prometheusMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		url := r.URL.String()
-		if strings.Contains(strings.ToLower(url), metricsHandlerPrefix) {
-			// pull metrics only for metrics handler
-			_ = mh.UpdateMetrics()
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-func startMetricsServer(c *config.ConfigHandler) *http.Server {
-
-	serverPort := c.GetServerPort()
-
-	router := mux.NewRouter()
-	router.Use(prometheusMiddleware)
-
-	reg := mh.GetRegistry()
-	router.Handle(metricsHandlerPrefix, promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
-	// pprof
-	router.Methods("GET").Subrouter().Handle("/debug/vars", expvar.Handler())
-	router.Methods("GET").Subrouter().HandleFunc("/debug/pprof/", pprof.Index)
-	router.Methods("GET").Subrouter().HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	router.Methods("GET").Subrouter().HandleFunc("/debug/pprof/profile", pprof.Profile)
-	router.Methods("GET").Subrouter().HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	router.Methods("GET").Subrouter().HandleFunc("/debug/pprof/trace", pprof.Trace)
-	router.Methods("GET").Subrouter().HandleFunc("/debug/pprof/allocs", pprof.Handler("allocs").ServeHTTP)
-	router.Methods("GET").Subrouter().HandleFunc("/debug/pprof/block", pprof.Handler("block").ServeHTTP)
-	router.Methods("GET").Subrouter().HandleFunc("/debug/pprof/heap", pprof.Handler("heap").ServeHTTP)
-	router.Methods("GET").Subrouter().HandleFunc("/debug/pprof/mutex", pprof.Handler("mutex").ServeHTTP)
-	router.Methods("GET").Subrouter().HandleFunc("/debug/pprof/goroutine", pprof.Handler("goroutine").ServeHTTP)
-	router.Methods("GET").Subrouter().HandleFunc("/debug/pprof/threadcreate", pprof.Handler("threadcreate").ServeHTTP)
-
-	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%v", serverPort),
-		Handler: router,
-	}
-
-	go func() {
-		logger.Log.Printf("serving requests on port %v", serverPort)
-		err := srv.ListenAndServe()
-		if err != http.ErrServerClosed {
-			log.Fatalf("ListenAndServe(): %v", err)
-		}
-		logger.Log.Printf("server on port %v shutdown gracefully", serverPort)
-	}()
-	return srv
-}
-
-func foreverWatcher() {
-	var srvHandler *http.Server
-	configPath := runConf.GetMetricsConfigPath()
-	directory := path.Dir(configPath)
-	os.MkdirAll(directory, 0755)
-	logger.Log.Printf("config directory for watch : %v", directory)
-
-	serverRunning := func() bool {
-		return srvHandler != nil
-	}
-
-	startServer := func() {
-		if !serverRunning() {
-			mh.InitConfig()
-			serverPort := runConf.GetServerPort()
-			logger.Log.Printf("starting server on %v", serverPort)
-			srvHandler = startMetricsServer(runConf)
-
-		}
-	}
-	stopServer := func() {
-		if serverRunning() {
-			logger.Log.Printf("stopping server")
-			srvCtx, srvCancel := context.WithTimeout(context.Background(), 5*time.Second)
-			if err := srvHandler.Shutdown(srvCtx); err != nil {
-				panic(err) // failure/timeout shutting down the server gracefully
-			}
-			srvCancel()
-			time.Sleep(1 * time.Second)
-			srvHandler = nil
-		}
-	}
-
-	// start server and listen for changes later
-	startServer()
-
-	// Create new watcher.
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		logger.Log.Fatal(err)
-	}
-	defer watcher.Close()
-
-	ctx := context.Background()
-	// Start listening for events.
-	go func() {
-		for ctx.Err() == nil {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
-				}
-				// k8s has to many cases to handle because of symlink, to be
-				// safe handle all cases
-				if event.Has(fsnotify.Create | fsnotify.Write | fsnotify.Remove | fsnotify.Rename) {
-					logger.Log.Printf("loading new config on %v", configPath)
-					// stop server if running
-					stopServer()
-					// start server
-					startServer()
-				}
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					logger.Log.Printf("error: %v", err)
-					return
-				}
-			}
-		}
-	}()
-
-	// Add a path.
-	err = watcher.Add(directory)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	logger.Log.Printf("starting file watcher for %v", configPath)
-
-	<-make(chan struct{})
-}
-
-// entryMain for exporter standalone
-func entryMain() {
-	logger.Init(utils.IsKubernetes())
-	defer func() {
-		if r := recover(); r != nil {
-			logger.Log.Printf("panic occured: %+v", r)
-			os.Exit(1)
-		}
-	}()
-
-	logger.Log.Printf("Version : %v", Version)
-	logger.Log.Printf("BuildDate: %v", BuildDate)
-	logger.Log.Printf("GitCommit: %v", GitCommit)
-
-	if (0 >= *agentGrpcPort) || (*agentGrpcPort > 65535) {
-		logger.Log.Printf("invalid agent-grpc-port exiting")
-		os.Exit(1)
-	}
-
-	svcHandler := metricsserver.InitSvcs()
-	go func() {
-		logger.Log.Printf("metrics service starting")
-		svcHandler.Run()
-		logger.Log.Printf("metrics service stopped")
-		os.Exit(0)
-	}()
-
-	runConf = config.NewConfigHandler(*metricsConfig, *agentGrpcPort)
-
-	mh, _ = metricsutil.NewMetrics(runConf)
-	mh.InitConfig()
-
-	gpuclient := gpuagent.NewAgent(mh, true)
-	if err := gpuclient.Init(); err != nil {
-		logger.Log.Printf("gpuclient init err :%+v", err)
-	}
-	defer gpuclient.Close()
-
-	go gpuclient.StartMonitor()
-
-	svcHandler.RegisterHealthClient(gpuclient)
-
-	foreverWatcher()
-}
 
 var (
 	metricsConfig = flag.String("amd-metrics-config", globals.AMDMetricsFile, "AMD metrics exporter config file")
@@ -242,6 +42,13 @@ var (
 func main() {
 	flag.Parse()
 
+	defer func() {
+		if r := recover(); r != nil {
+			logger.Log.Printf("panic occured: %+v", r)
+			os.Exit(1)
+		}
+	}()
+
 	if *versionOpt {
 		fmt.Printf("Version : %v\n", Version)
 		fmt.Printf("BuildDate: %v\n", BuildDate)
@@ -249,5 +56,19 @@ func main() {
 		os.Exit(0)
 	}
 
-	entryMain()
+	if (0 >= *agentGrpcPort) || (*agentGrpcPort > 65535) {
+		fmt.Printf("invalid agent-grpc-port exiting")
+		os.Exit(1)
+	}
+
+	logger.Init(utils.IsKubernetes())
+
+	logger.Log.Printf("Version : %v", Version)
+	logger.Log.Printf("BuildDate: %v", BuildDate)
+	logger.Log.Printf("GitCommit: %v", GitCommit)
+
+	exporterHandler := exporter.NewExporter(*agentGrpcPort, *metricsConfig)
+
+	exporterHandler.StartMain()
+
 }
